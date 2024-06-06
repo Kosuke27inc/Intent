fd7b3a63930df5c74bc6b0b9c85d7d88639ea3e6
Kosuke Miyazaki <27inc.kosukemiyazaki@gmail.com>
2024-06-06 18:43:43 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/kosuke/miyazaki/intent/MainActivity.kt
@@ -0,0 +1,26 @@
+package jp.techacademy.kosuke.miyazaki.intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.content.Intent
+import jp.techacademy.kosuke.miyazaki.intent.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
+    }
+}

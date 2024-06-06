52b1a4c8d81893a3fe849934c723ea702999b01a
Kosuke Miyazaki <27inc.kosukemiyazaki@gmail.com>
2024-06-06 18:59:45 +0900

    なぜかUpナビゲーション（戻るボタン）が出てこない。メンタリングでご確認いただく

--- app/src/main/java/jp/techacademy/kosuke/miyazaki/intent/SecondActivity.kt
+++ app/src/main/java/jp/techacademy/kosuke/miyazaki/intent/SecondActivity.kt
@@ -7,5 +7,13 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+            //戻るボタンを表示する
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        return true
     }
 }
